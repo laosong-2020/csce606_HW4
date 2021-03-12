41b321160621a2ff26e6a48bb1f16bbce4237781
ZhenleiSong <songzl0606@gmail.com>
2021-03-12 00:37:01 -0600

    fix

--- rottenpotatoes/spec/movie_spec.rb
+++ rottenpotatoes/spec/movie_spec.rb
@@ -5,7 +5,7 @@ describe Movie do
         movie1 = Movie.create(title: 'Test1', director: 'Director1')
         movie2 = Movie.create(title: 'Test2', director: 'Director1')
         results = Movie.similar_movies(movie1.title)
-        expect(results).to eql([movie1.title, movie1.title, movie2.title])
+        expect(results).to eql([movie1.title, movie2.title])
         #expect(results).to eql(["Test1", "Test1", "Test2"])
     end
     
@@ -15,9 +15,3 @@ describe Movie do
         expect(results).to eql(nil)
     end
 end
-
-  describe '.all_ratings' do
-    it 'returns all ratings' do
-      expect(Movie.all_ratings).to match(%w(G PG PG-13 NC-17 R))
-    end
-  end
