3890cd7110f65e09cd299c315812811afc32e2c0
ZhenleiSong <songzl0606@gmail.com>
2021-03-11 19:30:42 -0600

    add paths

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,8 +13,27 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-
+    when /^the home\s?page$/
+      movies_path
+      
+    when /^the edit page for "(.*)"$/ then
+       page_name =~ /^the edit page for "(.*)"$/
+       movie = Movie.find_by_title $1
+       edit_movie_path movie
+    
+    when /^the details page for "(.*)"$/ then
+       page_name =~ /^the details page for "(.*)"$/
+       movie = Movie.find_by_title $1
+       movie_path movie
+       
+    when /^the Similar Movies page for "(.*)"$/ then
+       page_name =~ /^the Similar Movies page for "(.*)"$/
+       movie = Movie.find_by_title $1
+       director_sames_path movie   
+    
+   
+    
+    
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
