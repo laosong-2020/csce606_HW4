5fdea713c7f9a36135d98361801544eccc7c74b9
ZhenleiSong <songzl0606@gmail.com>
2021-03-11 19:14:50 -0600

    add director column

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,5 +1,12 @@
 class Movie < ActiveRecord::Base
+    
+attr_accessible :title, :rating, :description, :release_date, :director
+
     def self.all_ratings
         return self.select("DISTINCT rating").map(&:rating)
+    end
+
+    def find_all_by_director
+        Movie.find_all_by_director(director)
     end
 end
